a37f6bd4c6d5459bbed946ccae48d6e54bdbf850
Nathan Henrique <ribeiro.nathan2002@gmail.com>
2024-09-16 15:24:40 -0300

    Adicionar arquivos de projeto.

---
+++ Ingresso_Heranca/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Ingresso
+{
+    public double Preco { get; set; }
+
+    public Ingresso(double preco)
+    {
+        Preco = preco;
+
+    }
+
+    public virtual void ExibirPreco()
+    {
+        Console.WriteLine("Ingresso normal: " + Preco);
+    }
+
+
+}
+
+public class VIP : Ingresso
+{
+    public double PrecoAdicional { get; set; }
+
+    public VIP(double preco, double precoAdicional) : base(preco)
+    {
+        PrecoAdicional = precoAdicional;
+    }
+
+    public override void ExibirPreco()
+    {
+        double precoVIP = Preco + PrecoAdicional;
+        Console.WriteLine("Ingresso VIP: " + precoVIP);
+    }
+
+}
+
+public class Normal : Ingresso
+{
+    public Normal(double preco) : base(preco)
+    {
+
+    }
+
+    public override void ExibirPreco()
+    {
+        Console.WriteLine("Preços do ingresso:");
+        base.ExibirPreco();
+    }
+}
+
+public class Principal
+{
+    public static void Main(string[] args)
+    {
+        Ingresso ingressoNormal = new Normal(50.0);
+        ingressoNormal.ExibirPreco();
+
+        Ingresso ingressoVIP = new VIP(50.0, 20.0);
+        ingressoVIP.ExibirPreco();
+
+    }
+
+}
